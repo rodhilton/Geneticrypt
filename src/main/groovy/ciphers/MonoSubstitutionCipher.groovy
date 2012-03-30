4782bedfc5e1413126f087646d60cd1e213f59d9
Rod Hilton <rod.hilton@gmail.com>
2012-03-29 20:11:01 -0600

    added cipher stuff

---
+++ src/main/groovy/ciphers/MonoSubstitutionCipher.groovy
@@ -0,0 +1,43 @@
+package ciphers
+
+class MonoSubstitutionCipher implements Cipher {
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    private String key
+    private Map<String, String> reverseMap
+    private Map<String, String> map
+
+    MonoSubstitutionCipher(String key) {
+
+        if(key.toList().sort().join("") != ALPHABET) throw new RuntimeException("Key ${key} invalid")
+
+        this.key = key
+        this.map = [:]
+        this.reverseMap = [:]
+        for (int i = 0; i < ALPHABET.length(); i++) {
+            map[ALPHABET[i]] = key[i]
+            reverseMap[key[i]] = ALPHABET[i]
+        }
+    }
+
+    @Override
+    String encrypt(String plaintext) {
+        plaintext.toUpperCase().toList().collect {plainChar ->
+            if (map.containsKey(plainChar)) {
+                map[plainChar]
+            } else {
+                plainChar
+            }
+        }.join("")
+    }
+
+    @Override
+    String decrypt(String ciphertext) {
+        ciphertext.toUpperCase().toList().collect {cipherChar ->
+            if (reverseMap.containsKey(cipherChar)) {
+                reverseMap[cipherChar]
+            } else {
+                cipherChar
+            }
+        }.join("")
+    }
+}
--- src/main/groovy/util/FrequencyAnalyzer.groovy
+++ src/main/groovy/util/FrequencyAnalyzer.groovy
@@ -40,17 +40,17 @@ public class FrequencyAnalyzer {
         texts.each { text ->
             for (int i = 0; i < text.length(); i++) {
                 String monoSub = safeSubstring(text, i, 1).toLowerCase();
-                if(monoSub ==~ /[a-z]{1}/) {
+                if (monoSub ==~ /[a-z]{1}/) {
                     monogramCounts[monoSub] += 1
                 }
 
                 String biSub = safeSubstring(text, i, 2).toLowerCase();
-                if(biSub ==~ /[a-z]{2}/) {
+                if (biSub ==~ /[a-z]{2}/) {
                     bigramCounts[biSub] += 1
                 }
 
                 String triSub = safeSubstring(text, i, 3).toLowerCase();
-                if(triSub ==~ /[a-z]{3}/) {
+                if (triSub ==~ /[a-z]{3}/) {
                     trigramCounts[triSub] += 1
                 }
             }
@@ -65,6 +65,15 @@ public class FrequencyAnalyzer {
         return text.substring(i, Math.min(i + i1, text.length()))
     }
 
+    /**
+     * Takes a map of ngram counts and transforms it into a map of counts based on percentage.
+     *
+     * Example: input is ["a": 3, "b": 2].  That means there are 5 total occurences of the ngrams, and a is 3 of those,
+     * which is 3/5 or 0.6, and b is 2 of them which is 2/5 or 0.4.  So the output is ["a": 0.6, "b": 0.4]
+     *
+     * @param ngramCounts
+     * @return tabulated map
+     */
     private Map<String, Double> tabulate(Map<String, Integer> ngramCounts) {
 
         final Double total = ngramCounts.values().inject(0) { sum, item -> sum + item}
